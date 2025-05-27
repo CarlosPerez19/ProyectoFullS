77ab6058798ed00588e8639fcd51610646402098
DNeekoD <carlos.perez02@epn.edu.ec>
2025-05-27 09:24:06 -0500

    Confirmar cuenta

--- Frontend/src/paginas/Confirmar.jsx
+++ Frontend/src/paginas/Confirmar.jsx
@@ -12,7 +12,7 @@ export const Confirmar = () => {
 
     const verifyToken = async () => {
         try {
-            const url = `${import.meta.env.VITE_BACKEND_URL}/confirmar/${token}`
+            const url = `${import.meta.env.VITE_BACKEND_URL}/confirmar-cuenta/${token}`
             const respuesta = await axios.get(url)
             setMensaje({ respuesta: respuesta.data.msg, tipo: false })
             setMostrarPassword(true)
